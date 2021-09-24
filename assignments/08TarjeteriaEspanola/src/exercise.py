45b6a40973de3c4ed07d4bb75fdc15803ad08ae2
luiscarlos55 <a01254044@itesm.mx>
2021-09-24 17:09:51 +0000

    Quiz de programación

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,7 +1,18 @@
+def tarjetas(x,y):
+    plumones = (x*12)
+    papel = (y*35)
+    if plumones<papel:
+        return(plumones)
+    elif papel<plumones:
+        return(papel)
 
 def main():
     #escribe tu código abajo de esta línea
-    
+    plumones=int(input('Dame la cantidad de pliegos de papel albanene: '))
+    papel=int(input('Dame la cantidad de plumones: '))
+    r=tarjetas(plumones,papel)
+    print('El número máximo de tarjetas que se pueden hacer es: '+str(r))
+
 
 if __name__=='__main__':
     main()
